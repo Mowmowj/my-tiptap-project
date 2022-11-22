4838705b5d1b26283ee9b389d47bb6a64c4f8e7e
shelton cheung <274334790@qq.com>
2022-11-22 18:25:58 +0800

    init app

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <TipTap />
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import TipTap from "./components/TipTap.vue";
+
+export default {
+  name: "App",
+  components: {
+    // HelloWorld,
+    TipTap,
+  },
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TipTap.vue
@@ -0,0 +1,49 @@
+<template>
+  <div v-if="editor">
+    <editor-content :editor="editor" />
+  </div>
+</template>
+
+<script>
+import StarterKit from "@tiptap/starter-kit";
+import { Editor, EditorContent } from "@tiptap/vue-3";
+
+import DraggableItem from "./DraggableItem.js";
+
+export default {
+  components: {
+    EditorContent,
+  },
+
+  data() {
+    return {
+      editor: null,
+    };
+  },
+
+  mounted() {
+    this.editor = new Editor({
+      extensions: [StarterKit, DraggableItem],
+      content: `
+        <p>This is a boring paragraph.</p>
+        <div data-type="draggable-item">
+          <div
+          >1Followed by a fancy draggable item.</div>
+        </div>
+        <div data-type="draggable-item">
+          <div 
+          >2Followed by a fancy draggable item.</div>
+        </div>
+        <div data-type="draggable-item">
+          <div 
+          >3Followed by a fancy draggable item</div>
+        </div>
+      `,
+    });
+  },
+
+  beforeUnmount() {
+    this.editor.destroy();
+  },
+};
+</script>
